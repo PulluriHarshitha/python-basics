1f6d8067a2ba228ab1cf8b2ba4508c4c66ef3506
pulluri harshitha <pulluriharshitha90@gmail.com>
2025-12-28 13:28:43 +0530

    change list item

---
+++ list.py
@@ -0,0 +1,52 @@
+#creation of list
+
+mylist = ["apple","banana","cherry"]
+print(mylist)  
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)   #allow duplicate values only
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(len(thislist))   #length of list
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1)
+print(list2)
+print(list3)   #list datatypes(int,str,bool)
+
+list1 = ["harshu", 20, True, "female"]  #allows list in all datatypes
+print(list1)
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))   #define the class using type
+
+#ACCESS THE LIST ITEMS
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[2])
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-3])  #negative indexing(starts from last)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])  # -1 is excluded
+
+#CHANGE LIST ITEMS
+
+thislist = ["c","python","java"]
+thislist[2] = "c++"
+print(thislist)
+
+thislist = ["c","c++","java","python","dsa"]
+thislist[1:4] = "html","css","javascript"
+print(thislist)
+
+thislist = ["c","c++","java","ml"]
+thislist.insert(4,"python")
+print(thislist)
