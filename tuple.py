4747d1dd5053fe61563f64a4837ca1f6ac3685df
pulluri harshitha <pulluriharshitha90@gmail.com>
2025-12-31 22:11:07 +0530

    different datatype

---
+++ tuple.py
@@ -0,0 +1,27 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)   #allow duplicates on tuple
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))  #length of tuple
+
+thistuple = ("apple",)
+print(type(thistuple))
+thistuple = ("apple")
+print(type(thistuple))  #tuple indicates with comma at last 
+
+tuple1 = ("JULY", "DEC", "JUN")
+tuple2 = (6,25,28)
+tuple3 = (True, False, False)
+print(tuple1)
+print(tuple2)
+print(tuple3)    #uple items can be of any data type
+
+tuple1 = ("Harshu", 19, True,  "female")
+print(tuple1)   #tuple can contain different data types
+
+
+
+
