446017644f564f47a96df38a35b5c80f846f046d
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-01 22:39:29 +0530

    access tuples

--- tuple.py
+++ tuple.py
@@ -17,13 +17,31 @@ tuple2 = (6,25,28)
 tuple3 = (True, False, False)
 print(tuple1)
 print(tuple2)
-print(tuple3)    #uple items can be of any data type
+print(tuple3)    #tuple items can be of any data type
 
 tuple1 = ("Harshu", 19, True,  "female")
 print(tuple1)   #tuple can contain different data types
 
 thistuple = tuple(("sun", "mon", "sat"))  # note the double round-brackets as constructor
 print(thistuple)
+
+#ACCESS TUPLE ITEMS
+
+thistuple = ("JAN", "FEB", "MAR")
+print(thistuple[1])
+
+thistuple = ("JUL", "JUN", "AUG")
+print(thistuple[-1])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("JAN", "FEB", "MAR", "APR", "JUN", "JUL", "AUG")
+print(thistuple[2:5])
+
+       #UPDATE TUPLES
+
+
 
 
 
