87ff1f233c43b5492feedb7a31d35db061b5e9b9
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-03 22:49:11 +0530

    ACCESS SET ITEMS

--- sets.py
+++ sets.py
@@ -13,3 +13,39 @@ print(thisset)  #The values False and 0 are considered the same value in sets, a
 
 thisset = {"sundari", "vishwa"}
 print(len(thisset))   #Length of a Set
+
+set1 = {"vishwa", "sundari", "justmarried"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+print(set1)
+print(set2)
+print(set3)   #Set Items - Data Types
+
+set1 = {"vishwa", 34, True, 40, "male"}
+print(set1)  #different data types
+
+a = 10
+print(type(a))      # <class 'int'>
+b = "hello"
+print(type(b))      # <class 'str'>
+c = [1, 2, 3]
+print(type(c))      # <class 'list'>  type() is used to find the data type (class) of a variable.
+
+thisset = set(("bala", "vishwa", "justmarried"))
+print(thisset)  # constructor the set list is unordered, so the result will display the items in a random order.
+
+
+   #ACCESS SET ITEMS 
+
+thisset = {"vishwa", "bala", "justmarried"}
+for x in thisset:
+  print(x)   #set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
+
+
+thisset = {"vishwa", "bala", "justmarried"}
+print("vishwa" in thisset)   #Check if "vishwa" is present in the set
+
+
+thisset = {"vishwa", "sundari", "justmarried"}
+print("vishwa" not in thisset)
+
