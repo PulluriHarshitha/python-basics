06b276b99a284a96c5f7118f499ec31d2b91dc85
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-05 21:50:04 +0530

    brand a dictionary

---
+++ Dictionaries.py
@@ -0,0 +1,13 @@
+thisdict = {
+  "brand": "pulluri",
+  "model": "harshu",
+  "year": 2005
+}
+print(thisdict)  #Create and print a dictionary
+
+thisdict = {
+  "brand": "pulluri",
+  "model": "harshu",
+  "year": 2025
+}
+print(thisdict["model"])   #the "brand" value of the dictionary
