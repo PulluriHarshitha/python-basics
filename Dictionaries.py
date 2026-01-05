416d755168ff34f92e6c5c51f176d4e6d7e0d52d
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-05 21:58:14 +0530

    dict duplicates

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,13 +1,25 @@
 thisdict = {
-  "brand": "pulluri",
-  "model": "harshu",
+  "name": "harshu",
+  "age": "20",
   "year": 2005
 }
 print(thisdict)  #Create and print a dictionary
 
 thisdict = {
-  "brand": "pulluri",
-  "model": "harshu",
+  "name": "harshu",
+  "city": "hyderabad",
   "year": 2025
 }
-print(thisdict["model"])   #the "brand" value of the dictionary
+print(thisdict["city"])   #the "city" value of the dictionary
+
+thisperson = {
+  "name": "harshu",
+  "city": "Mumbai",
+  "year": 2005,
+  "year": 2025,
+  "name": "rohitreddy"
+}
+print(thisdict)   #Dictionaries cannot have two items with the same key
+
+
+
