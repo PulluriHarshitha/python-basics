5d29a685952c224d332a7474b8a0e2583c5e1d08
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-07 22:26:37 +0530

    if statenent

--- functions.py
+++ functions.py
@@ -21,3 +21,31 @@ if x > 5:
     print("x is greater than 5")
     print("Condition is true")
     print("Value of x:", x)   #multiple satements inside one
+
+  #ELIF KEYWORD
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+ 
+x = 20
+if x > 0:
+    print("Positive number")
+elif x == 0:
+    print("Zero")
+else:
+    print("Negative number")
+
+
+age = 16
+if age >= 18:
+    print("Eligible to vote")
+elif age >= 13:
+    print("Teenager")
+    print("Not eligible to vote")
+else:
+    print("Child")   #multiple statements elif
+
