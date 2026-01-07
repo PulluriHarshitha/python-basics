8649d653f8801b9cb3b29592a95a2640341c615e
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-07 22:10:50 +0530

    if statenent

---
+++ functions.py
@@ -0,0 +1,23 @@
+#PYTHON IF STATEMENTS
+a = 10
+b = 5
+print(a != b)  #not equal to 
+print(a == b)  #equal to
+print(a > b)  #greater than
+print(a < b)  #less than
+print(a <= b)   #lessthan or equal
+print(a >= b)    #greater than or equal
+
+age = 18
+if age >= 18:
+    print("You are eligible to vote")
+
+number = 50
+if number > 0:
+  print("The number is positive")
+
+x = 10
+if x > 5:
+    print("x is greater than 5")
+    print("Condition is true")
+    print("Value of x:", x)   #multiple satements inside one
