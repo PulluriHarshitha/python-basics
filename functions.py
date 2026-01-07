f4169fdf002c0ea10ed63ca708705b7fcc156065
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-07 22:29:32 +0530

    weak cheaker

--- functions.py
+++ functions.py
@@ -49,3 +49,20 @@ elif age >= 13:
 else:
     print("Child")   #multiple statements elif
 
+#weak cheacker
+
+day = 6
+if day == 1:
+  print("Monday")
+elif day == 2:
+  print("Tuesday")
+elif day == 3:
+  print("Wednesday")
+elif day == 4:
+  print("Thursday")
+elif day == 5:
+  print("Friday")
+elif day == 6:
+  print("Saturday")
+elif day == 7:
+  print("Sunday")
