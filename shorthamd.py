a4031dd14616aa6e59591af6d2fe1cf89f49455d
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-10 23:06:13 +0530

    shorthand

---
+++ shorthamd.py
@@ -0,0 +1,16 @@
+#syntax : value_if_true if condition else value_if_false
+
+#simple example
+a = 50
+b = 30
+print("a is greater") if a > b else print("b is greater")
+
+#assigning a value
+age = 18
+status = "Adult" if age >= 18 else "Minor"
+print(status)
+
+#even or odd
+num = 6
+result = "Even" if num % 2 == 0 else "Odd"
+print(result)
