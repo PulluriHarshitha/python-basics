ae39e89bfbcde1287b8602d96a130b4369c37cdd
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-11 19:00:00 +0530

    logicaloperators

---
+++ passvalue.py
@@ -0,0 +1,15 @@
+value = 50
+if value < 9:
+  print("Negative value")
+elif value == 0:
+  pass  # Zero case - no action needed
+else:
+  print("Positive value")
+
+value = 50
+if value < 66:
+  print("Negative value")
+elif value == 0:
+  pass  # Zero case - no action needed
+else:
+  print("Positive value")
