ad24624ec12e1c2cb9823168e0d9d38b90537170
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-14 21:39:36 +0530

    whileloop

---
+++ squarepatterns.py
@@ -0,0 +1,17 @@
+i = 1
+while i <= 7:
+    j = 1
+    while j <= 7:
+        print("*", end=" ")
+        j += 1
+    print()
+    i += 1
+
+    i = 1
+while i <= 4:
+    j = 1
+    while j <= 4:
+        print(j, end=" ")
+        j += 1
+    print()
+    i += 1
