dd71c65b2ce4edd1774654ab74951a748d39e1b8
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-15 21:19:09 +0530

    patterns

--- example.py
+++ example.py
@@ -29,3 +29,34 @@ while i <= 4:
         j += 1
     print()
     i += 1
+
+#lowecase alphabets
+i = 1
+while i <= 4:
+    j = 0
+    while j < i:
+        print(chr(97 + j), end=" ")
+        j += 1
+    print()
+    i += 1
+
+#same lowercase alphabets in eachrow
+i = 0
+while i < 4:
+    j = 0
+    while j <= i:
+        print(chr(97 + i), end=" ")
+        j += 1
+    print()
+    i += 1
+
+#pattern using name letters
+name = "HARSHITHA"
+i = 1
+while i <= 9:
+    j = 0
+    while j < i:
+        print(name[j], end=" ")
+        j += 1
+    print()
+    i += 1
