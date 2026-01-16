84281b71d171722d10e327d18ba3bd63b59f7275
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-16 22:37:54 +0530

    patterns

--- example.py
+++ example.py
@@ -116,5 +116,15 @@ for i in range(4, 0, -1):   #inverted triangle
         print("*", end=" ")
     print()
 
+n = 4
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    for j in range(i):
+        print("* ", end="")   #pyramid pattern
+    print()
 
+for i in range(4):
+    for j in range(i + 1):
+        print(chr(65 + j), end=" ")    #alphabet pattern
+    print()
 
