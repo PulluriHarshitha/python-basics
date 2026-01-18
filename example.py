f8f06a9eaeebae46884077d0da19ddfc699020d4
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-18 21:01:36 +0530

    patterns

--- example.py
+++ example.py
@@ -128,3 +128,47 @@ for i in range(4):
         print(chr(65 + j), end=" ")    #alphabet pattern
     print()
 
+    n = 4
+
+# Upper part
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    for j in range(i):
+        print("* ", end="")
+    print()
+
+# Lower part
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i), end="")
+    for j in range(i):
+        print("* ", end="")
+    print()
+
+
+n = 5
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n-1 or j == 0 or j == n-1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+
+n = 5
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        if j == 1 or j == i or i == n:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+n = 4
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
