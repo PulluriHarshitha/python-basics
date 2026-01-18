58cf9ac20be1b730d005da9effce7fda0c4e468d
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-18 21:04:39 +0530

    patterns

--- example.py
+++ example.py
@@ -171,4 +171,39 @@ for i in range(1, n + 1):
         print(j, end=" ")
     print()
 
+for i in range(1, 6):
+    for j in range(i):
+        if i % 2 == 0:
+            print("*", end=" ")
+        else:
+            print(i, end=" ")
+    print()
+
+
+num = 1
+for i in range(1, 5):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
+
+n = 3
+
+# Upper part
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print(" " * (2 * (n - i) * 2), end="")
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
+
+# Lower part
+for i in range(n - 1, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print(" " * (2 * (n - i) * 2), end="")
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
 
