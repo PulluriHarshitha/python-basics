5583c397182668bc02ea8d7b1a07ff0138f0384a
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-20 17:42:23 +0530

    patterns

--- example.py
+++ example.py
@@ -252,3 +252,17 @@ for i in range(1, n + 1):
         else:
             print(" ", end="")
     print()
+
+
+
+n = 10
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    for j in range(i):
+        print("* ", end="")
+    print()
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i), end="")
+    for j in range(i):
+        print("* ", end="")
+    print()
