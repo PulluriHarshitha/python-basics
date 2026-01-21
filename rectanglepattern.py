f4e793f170904533c4789b9f0b53c14a647351be
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-21 22:47:41 +0530

    patterns

--- rectanglepattern.py
+++ rectanglepattern.py
@@ -32,3 +32,24 @@ for i in range(rows):
     for j in range(1,cols+1):
         print(j, end=" ")
     print()
+
+#contionous num rectangle
+rows = 8
+cols = 16
+num = 1
+for i in range(rows):
+    for j in range(cols):
+        print(num, end=" ")
+        num += 1
+    print()
+
+    #hollow rectangle
+rows = 5
+cols = 10
+for i in range(rows):
+    for j in range(cols):
+        if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+            print(6, end=" ")
+        else:
+            print(" ", end=" ")
+    print()
