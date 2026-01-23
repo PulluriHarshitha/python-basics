10aea3a386b096644925d7aace337cfab692ca17
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-23 21:12:41 +0530

    Continuous Number Triangle

--- trianglepattern.py
+++ trianglepattern.py
@@ -12,3 +12,11 @@ for i in range(1, n + 1):
         print(j, end=" ")
     print()
 
+#Continuous Number Triangle
+n = 6
+num = 1
+for i in range(1, n + 1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
