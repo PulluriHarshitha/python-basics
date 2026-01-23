57a0524834aed0ffd2df60fc4e21af584683cf01
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-23 21:24:56 +0530

    CONTIONOUS ALPHABET TRIANGLE

--- trianglepattern.py
+++ trianglepattern.py
@@ -37,3 +37,12 @@ for i in range(n):
         print(chr(65 + j), end=" ")
     print()
 
+#Continuous Alphabet Triangle
+
+n = 7
+ch = 65
+for i in range(1, n + 1):
+    for j in range(i):
+        print(chr(ch), end=" ")
+        ch += 1
+    print()
