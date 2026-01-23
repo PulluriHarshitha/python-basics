7ca4782d7169200dfe589de59cde0c87314abf51
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-23 21:44:34 +0530

    trianglepatterns.py

--- trianglepattern.py
+++ trianglepattern.py
@@ -61,3 +61,9 @@ for i in range(n,0,-1):
     for j in range(i):
         print(chr(65 + i), end = " ")
     print()
+
+n = 5
+for i in range(n, 0, -1):
+    for j in range(i):
+        print("#", end=" ")
+    print()
