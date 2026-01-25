69100f8fa5c2ab6f8fd573ea975f760dd27a2ffc
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-01-25 22:24:36 +0530

    streak.py

--- streak.py
+++ streak.py
@@ -1,20 +1,26 @@
 ch = input("Enter a character: ")
-
 if ch in "aeiouAEIOU":
     print("Vowel")
 else:
     print("Consonant")
 
+
 num = int(input("Enter a number: "))
 temp = num
 rev = 0
-
 while num > 0:
     digit = num % 10
     rev = rev * 10 + digit
     num = num // 10
-
 if temp == rev:
     print("Palindrome number")
 else:
     print("Not a palindrome")
+
+
+text = input("Enter a string: ")
+if text == text[::-1]:
+    print("Palindrome string")
+else:
+    print("Not a palindrome")
+
