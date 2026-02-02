ddac41cd10e09e5eec070bc4d9935735080ae5b2
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-02-02 21:27:29 +0530

    function parameters

--- pythonfunctions.py
+++ pythonfunctions.py
@@ -2,5 +2,14 @@
 
 def fun():
     print("Welcome to Function")
-
 fun() 
+
+#function parameters
+
+def evenOdd(x):
+    if (x % 2 == 0):
+        return "Even"
+    else:
+        return "Odd"
+print(evenOdd(16))
+print(evenOdd(7))
