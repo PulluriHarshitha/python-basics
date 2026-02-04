129724a032d817a272f2e6b5ea3e004bc6203730
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-02-04 21:42:09 +0530

    TIME-DISTANCE CALCULATION USING abs() FUNCTIONS

--- builtinfunctions.py
+++ builtinfunctions.py
@@ -11,3 +11,38 @@ print('Absolute value of float is:',abs(float_number))
 
 complex_number = (3 - 5j)
 print('Absolute value or Magnitude of complex is:', abs(complex_number))
+
+
+#time-distance calculation using py.abs() function
+#Distance  = Speed * Time
+#Time = Distance / Speed
+#Speed = Distance / Time
+#above formulaes used in prgm
+
+# Function to calculate speed
+def cal_speed(dist, time):
+    print(" Distance(km) :", dist)
+    print(" Time(hr) :", time)
+    return dist / time
+# Function to calculate distance traveled
+def cal_dis(speed, time):
+    print(" Time(hr) :", time)
+    print(" Speed(km / hr) :", speed)
+    return speed * time
+# Function to calculate time taken
+def cal_time(dist, speed):
+    print(" Distance(km) :", dist)
+    print(" Speed(km / hr) :", speed)
+    return dist / speed
+# Driver Code
+# Calling function cal_speed()
+print(" The calculated Speed(km / hr) is :",
+      cal_speed(abs(56), abs(5)))
+print("")
+# Calling function cal_dis()
+print(" The calculated Distance(km) :",
+      cal_dis(abs(62), abs(-2)))
+print("")
+# Calling function cal_time()
+print(" The calculated Time(hr) :",
+      cal_time(abs(48), abs(4.5)))
