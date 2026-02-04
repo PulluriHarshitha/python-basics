44a6cd7e796db11c9a949f03690e0fd0338d2446
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-02-04 21:24:20 +0530

    abstract complex number

--- builtinfunctions.py
+++ builtinfunctions.py
@@ -2,3 +2,12 @@
 # An integer
 var = 3150
 print('Absolute value of integer is:', abs(var))
+
+#floating point number
+float_number = -1234
+print('Absolute value of float is:',abs(float_number))
+
+#abs() a complex number
+
+complex_number = (3 - 5j)
+print('Absolute value or Magnitude of complex is:', abs(complex_number))
