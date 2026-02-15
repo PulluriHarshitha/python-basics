5904aa709e4087a78eea2088cb0a0a74c38e3703
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-02-15 19:59:44 +0530

    prime numbers blw two numbers

--- streak.py
+++ streak.py
@@ -296,3 +296,14 @@ def valid(board, row, col, num):
             if board[i][j] == num:
                 return False
     return True
+
+#prime numbers blw two numbers
+
+a, b = map(int, input("Enter range: ").split())
+for n in range(a, b+1):
+    if n > 1:
+        for i in range(2, int(n**0.5)+1):
+            if n % i == 0:
+                break
+        else:
+            print(n, end=" ")
