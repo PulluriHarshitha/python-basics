f7d6af031cfb1ae602e3f9f111978cba2ba5b957
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-02-18 21:46:30 +0530

    simple calculator

--- streak.py
+++ streak.py
@@ -255,3 +255,21 @@ for n in range(a, b+1):
                 break
         else:
             print(n, end=" ")
+
+
+#simple calculator 
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("1.Add  2.Sub  3.Mul  4.Div")
+choice = int(input("Choose operation: "))
+if choice == 1:
+    print("Result =", a + b)
+elif choice == 2:
+    print("Result =", a - b)
+elif choice == 3:
+    print("Result =", a * b)
+elif choice == 4:
+    print("Result =", a / b)
+else:
+    print("Invalid choice")
