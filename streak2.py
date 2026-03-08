b62f619a6c1c1dd89d9090ddac132207e310f74d
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-03-08 22:12:47 +0530

    range

--- streak2.py
+++ streak2.py
@@ -270,5 +270,10 @@ name ="harshu"
 print(name)
 print(type(name))
 
-
-#
+#range
+n = int(input("Enter number of terms: "))
+a = 0
+b = 1
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
