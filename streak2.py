9e09d0c0942f96ff99b8d9641e3e8f557977c625
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-03-09 22:40:44 +0530

    GCD

--- streak2.py
+++ streak2.py
@@ -277,3 +277,12 @@ b = 1
 for i in range(n):
     print(a, end=" ")
     a, b = b, a + b
+
+#Program to Find GCD of Two Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+while b != 0:
+    temp = b
+    b = a % b
+    a = temp
+print("GCD is:", a)
