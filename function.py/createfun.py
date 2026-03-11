65c8d28303483e76af4c445e89b06b082427c4ea
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-03-11 22:29:55 +0530

    create a fun()

---
+++ function.py/createfun.py
@@ -0,0 +1,10 @@
+def my_function():
+    print("hello function")
+my_function()
+
+#calling a function
+def my_function():
+    print("hello function from vs")
+    
+    my_function()
+    my_function()
