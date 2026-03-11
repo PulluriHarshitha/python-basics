f16efeffc359ed2d69edf836b1aff794d3e14a29
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-03-11 22:48:09 +0530

    return values

---
+++ function.py/return values.py
@@ -0,0 +1,10 @@
+#function with return statement
+def get_greeting():
+    return "good morning"
+message = get_greeting()
+print(message)
+
+#function with return value directly
+def get_greeting():
+    return "good morning"
+print(get_greeting())
