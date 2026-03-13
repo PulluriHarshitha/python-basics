1ecf6888620cd6ef04fc6e490c2cf10e7b7c779b
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-03-13 23:10:58 +0530

    example

--- streak.py
+++ streak.py
@@ -371,3 +371,12 @@ print("Missing =", expected - sum(arr))
 
 #example
   print("hello world");
+
+#EXAMPLE
+num = int(input("Enter a number: "))
+fact = 1
+
+for i in range(1, num + 1):
+    fact = fact * i
+
+print("Factorial =", fact)  
