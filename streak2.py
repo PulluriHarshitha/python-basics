bb296de6a8a623ec33aa2686b20ade19d97e9afd
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-03-17 19:12:17 +0530

    simple calculator

--- streak2.py
+++ streak2.py
@@ -339,3 +339,27 @@ if text == text[::-1]:
     print("Palindrome")
 else:
     print("Not Palindrome")
+
+#simple calculator 
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return a / b
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("1.Add 2.Sub 3.Mul 4.Div")
+choice = int(input("Enter choice: "))
+if choice == 1:
+    print(add(a, b))
+elif choice == 2:
+    print(sub(a, b))
+elif choice == 3:
+    print(mul(a, b))
+elif choice == 4:
+    print(div(a, b))
+else:
+    print("Invalid choice")
