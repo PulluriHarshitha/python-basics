bed88035bdace0778ad9fb4d2c3b4ca4d03ef80b
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-03-18 22:57:04 +0530

    matrix addition

--- streak2.py
+++ streak2.py
@@ -376,3 +376,17 @@ while True:
     else:
         print("Correct!")
         break
+
+#matrix addition
+A = [[1, 2], [3, 4]]
+B = [[5, 6], [7, 8]]
+
+result = [[0, 0], [0, 0]]
+
+for i in range(2):
+    for j in range(2):
+        result[i][j] = A[i][j] + B[i][j]
+
+print("Result:")
+for row in result:
+    print(row)
