d903471c3bdf3e84c8474116168c21552652559c
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-03-21 21:58:09 +0530

    ACCESSING INDIVIDUAL ARGUMENTS

--- function.py/args_kwargs.py
+++ function.py/args_kwargs.py
@@ -1,3 +1,13 @@
 def my_function(*kids):
   print("The youngest child is " + kids[1])
 my_function("harshu", "sweetie", "kanamma")
+
+
+#accessing indiviual arguments
+def my_function(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("Third argument:", args[2])
+  print("All arguments:", args)
+my_function("harshu","sweetie","ammu")
