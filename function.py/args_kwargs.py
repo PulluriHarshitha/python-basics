aa97e97335b1685ff74e4df076069ff28e79bc16
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-03-23 22:26:27 +0530

    upadate

--- function.py/args_kwargs.py
+++ function.py/args_kwargs.py
@@ -28,3 +28,12 @@ def my_function(*numbers):
 print(my_function(1, 2, 3))
 print(my_function(10, 20, 30, 40))
 print(my_function(5))
+
+def my_function(*numbers):
+  total = 0
+  for num in numbers:
+    total += num
+  return total
+print(my_function(5,6,7,8,9,10))
+print(my_function(50,60,70,80,90,100))
+print(my_function(10))
