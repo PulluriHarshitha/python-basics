d7877d32ab4504c033b04d4b4e9fdfd179c35b5d
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-03-27 19:44:53 +0530

    Update trianglepattern.py

--- trianglepattern.py
+++ trianglepattern.py
@@ -68,3 +68,12 @@ for i in range(n, 0, -1):
     for j in range(i):
         print("#", end=" ")
     print()
+
+#INVERTED # TRIANGLE 
+n = 6
+for i in range(n, 0, -1):
+    for j in range(i):
+        print("#", end=" ")
+    print()
+
+
