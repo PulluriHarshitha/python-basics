878a87d4faab1673d15630ea3c01a6eb13bab354
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-04-04 20:53:54 +0530

    Update streak.py

--- streak.py
+++ streak.py
@@ -29,7 +29,7 @@ else:
  flag = 0
 if num > 1:
     for i in range(2, num):
-#         if num % i == 0:
+       if num % i == 0:
 #             flag = 1
 #             break
 #         if flag == 0:
