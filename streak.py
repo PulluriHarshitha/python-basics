870cbfa8248c20de6f93991f38d058dc31d000e1
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-04-04 20:53:21 +0530

    Update streak.py

--- streak.py
+++ streak.py
@@ -27,8 +27,8 @@ else:
  #prime number
  num = int(input("Enter a number: "))
  flag = 0
-# if num > 1:
-#     for i in range(2, num):
+if num > 1:
+    for i in range(2, num):
 #         if num % i == 0:
 #             flag = 1
 #             break
