67ebf470814f91d57d472b0a12a8d3679900bd76
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-04-04 20:54:29 +0530

    Update streak.py

--- streak.py
+++ streak.py
@@ -30,8 +30,8 @@ else:
 if num > 1:
     for i in range(2, num):
        if num % i == 0:
-#             flag = 1
-#             break
+            flag = 1
+             break
 #         if flag == 0:
 #          print("Prime number")
 #     else:
