bc9bb9b4d0f5e06d36f9e4562ebeab4c70db3004
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-04-06 22:07:33 +0530

    Update streak.py

--- streak.py
+++ streak.py
@@ -12,9 +12,9 @@ else:
 while num > 0:
     digit = num % 10
     rev = rev * 10 + digit
-#     num = num // 10
-# if temp == rev:
-#     print("Palindrome number")
+     num = num // 10
+if temp == rev:
+     print("Palindrome number")
 # else:
 #     print("Not a palindrome")
 
