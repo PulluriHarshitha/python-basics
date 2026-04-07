73931f8d2789e0e64e0026047e9d99301456f7e0
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-04-07 21:48:30 +0530

    07-26

--- streak.py
+++ streak.py
@@ -184,9 +184,8 @@ sum = 0
 # else:
 #     print("Wrong! Number was:", num)
 
-#file handlingg
-
- with open("data.txt", "w") as f:
+#file handling
+with open("data.txt", "w") as f:
     f.write("Python streak day 9")
 
 with open("data.txt", "r") as f:
