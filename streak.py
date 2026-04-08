d111c963a71ef41688684560c443191a1d5064ad
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-04-08 22:42:53 +0530

    Update streak.py

--- streak.py
+++ streak.py
@@ -55,8 +55,8 @@ else:
 
 ##fibonacci series
 n = int(input("Enter number of terms: "))
-# a, b = 0, 1
-# for i in range(n):
+a, b = 0, 1
+for i in range(n):
 #     print(a, end=" ")
 #     a,b = b,a+b
     
