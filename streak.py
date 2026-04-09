a8569b4fbe4f13bc1c5e63c157da3ed337622468
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-04-09 22:49:18 +0530

    Update streak.py

--- streak.py
+++ streak.py
@@ -80,7 +80,7 @@ for i in range(n):
 days = ["Monday", "Tuesday", "Wednesday", "Thursday",
          "Friday", "Saturday", "Sunday"]
    for day in days:
-#     print(day)
+    print(day)
 # #months in a year
 # months = ["January", "February", "March", "April", "May", "June",
 #           "July", "August", "September", "October", "November", "December"]
