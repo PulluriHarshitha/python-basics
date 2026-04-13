cb5f8b4f4903e391c54d2d12e8bbf788b93c83c2
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-04-13 22:19:39 +0530

    Update streak.py

--- streak.py
+++ streak.py
@@ -136,16 +136,16 @@ else:
 
 # #fibonacci series
 n = int(input("Enter number of terms: "))
- a, b = 0, 1
+a, b = 0, 1
 print("Fibonacci Series:")
  for i in range(n):
    print(a, end=" ")
      a, b = b, a + b
 
 
-# #recursive fibonacci program
-# def fibonacci(n):
-#     if n <= 1:
+##recursive fibonacci program
+def fibonacci(n):
+     if n <= 1:
 #         return n
 #     return fibonacci(n - 1) + fibonacci(n - 2)
 # n = int(input("Enter number of terms: "))
