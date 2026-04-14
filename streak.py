8d058eeccb09826a9b1a2d5ee0dbc4a6740fb915
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-04-14 22:04:43 +0530

    streak

--- streak.py
+++ streak.py
@@ -147,7 +147,7 @@ a, b = b, a + b
 def fibonacci(n):
      if n <= 1:
         return n
-#     return fibonacci(n - 1) + fibonacci(n - 2)
+     return fibonacci(n - 1) + fibonacci(n - 2)
 # n = int(input("Enter number of terms: "))
 # print("Fibonacci Series:")
 # for i in range(n):
