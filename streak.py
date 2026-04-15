10b649a7ee7305489afb261e62bf86fb40dbff7e
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-04-15 23:04:29 +0530

    15-04-26

--- streak.py
+++ streak.py
@@ -158,7 +158,7 @@ for i in range(n):
 def fibonacci(n):
     a, b = 0, 1
     for _ in range(n):
-#         print(a, end=" ")
+        print(a, end=" ")
 #         a, b = b, a + b
 
 # n = int(input("Enter number of terms: "))
