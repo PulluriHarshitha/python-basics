9f9c31a63da36814fc9ef8fd71aa861a3ec834a0
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-04-16 23:11:53 +0530

    16-04-26

--- streak.py
+++ streak.py
@@ -172,7 +172,7 @@ print("Min:", min(list))
 ##number guessing game
 import random
 num = random.randint(1, 10)
-# guess = int(input("Guess number (1-10): "))
+guess = int(input("Guess number (1-10): "))
 # if guess == num:
 #     print("Correct!")
 # else:
