f3271189302d50004555b6e4fcc384b2b0141a3d
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-04-18 22:10:08 +0530

    18-04-26

--- streak2.py
+++ streak2.py
@@ -177,7 +177,7 @@ if temp == rev:
 # #number guessing game
 # import random
 # num = random.randint(1, 10)
-# guess = int(input("Guess number (1-10): "))
+ guess = int(input("Guess number (1-10): "))
  if guess == num:
     print("Correct!")
  else:
