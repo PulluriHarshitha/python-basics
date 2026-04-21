694dbff4d4acb2f74aa0989c80bf51b24ebf193f
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-04-21 20:47:16 +0530

    21-04-26

--- streak2.py
+++ streak2.py
@@ -159,10 +159,10 @@ text = input("Enter a string: ")
 
 
 # #fiboonaci series vth loop method
-# def fibonacci(n):
-#     a, b = 0, 1
-#     for _ in range(n):
-print(a, end=" ")
+def fibonacci(n):
+a, b = 0, 1
+for _ in range(n):
+ print(a, end=" ")
 
 a, b = b, a + b
 n = int(input("Enter number of terms: "))
