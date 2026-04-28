117f04964b26b0663483136d8366b947c93ea803
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-04-28 21:01:37 +0530

    28-04-26

--- streak2.py
+++ streak2.py
@@ -22,7 +22,7 @@ text = input("Enter a string: ")
 if text == text[::-1]:
      print("Palindrome string")
 else:
-#     print("Not a palindrome")
+     print("Not a palindrome")
 
 # #prime number
 # num = int(input("Enter a number: "))
