aaa870d266e36285967387c967d860bd3a237e04
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-04-28 20:47:39 +0530

    28-04-26

--- streak2.py
+++ streak2.py
@@ -102,10 +102,10 @@ b = int(input())
 c = int(input())
 if a >= b and a >= c:
      print(a)
-     elif b >= c:
-print(b)
+elif b >= c:
+     print(b)
 else:
-print(c)
+     print(c)
 
 #simple prime number
 n = int(input())
