e72cbea3d32386034a4da49cecaab297cfd137c6
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-04-28 21:58:57 +0530

    28-04-2026

--- streak2.py
+++ streak2.py
@@ -27,7 +27,7 @@ else:
 # #prime number
 num = int(input("Enter a number: "))
 flag = 0
-# if num > 1:
+if num > 1:
 #     for i in range(2, num):
 #         if num % i == 0:
 #             flag = 1
