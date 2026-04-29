a6c8bb79ae0d5189b5e1570e2ffa8c4f96132a79
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-04-29 22:17:16 +0530

    29-04-2026

--- streak2.py
+++ streak2.py
@@ -92,7 +92,7 @@ months = ["January", "February", "March", "April", "May", "June",
            "July", "August", "September", "October", "November", "December"]
 if 1 <= month <= 12:
      print("Month is:", months[month - 1])
-# else:
+else:
 #     print("Invalid input")
 
 
