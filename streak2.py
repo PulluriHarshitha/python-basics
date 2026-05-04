e4403456d19426164c78fa30bf42b5ab59c38d10
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-05-04 21:50:53 +0530

    04-05-2026

--- streak2.py
+++ streak2.py
@@ -47,7 +47,7 @@ while num > 0:
      digit = num % 10
      sum = sum + digit**3
     num = num // 10
-# if temp == sum:
+     if temp == sum:
 #     print("Armstrong number")
 # else:
 #     print("Not an Armstrong number")
