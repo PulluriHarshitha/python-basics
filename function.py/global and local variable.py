0bcc5f94ae1c7fe8b7ec074a214a1b35c995bf6c
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-05-19 13:13:54 +0530

    19-05-26

---
+++ function.py/global and local variable.py
@@ -0,0 +1,2 @@
+#Variables store data in memory and scope defines the specific region of a program where a variable is accessible. It dictates the visibility and lifetime of the variable within the source code. Variables are categorized into two primary scopes: Global and Local.
+#Local Variable: Local variables are defined inside a function and exist only during its execution. They cannot be accessed from outside the function.
