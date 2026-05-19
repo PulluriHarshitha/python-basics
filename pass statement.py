5a99df632cef915c95c56638e269e8dbed518e42
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-05-19 12:41:10 +0530

    19-05-26

--- pass statement.py
+++ pass statement.py
@@ -1,4 +1,6 @@
+#The pass keyword in a function is used when we define a function but don't want to implement its logic immediately. 
+#It allows the function to be syntactically valid, even though it doesn't perform any actions yet.
+
 def fun():
     pass
-
 fun() # Call the function
