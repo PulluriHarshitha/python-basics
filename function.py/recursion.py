dce07364e85ea957aee9deabe8ad1cf68250db11
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-05-20 18:42:30 +0530

    20-05-26

--- function.py/recursion.py
+++ function.py/recursion.py
@@ -29,3 +29,13 @@ def factorial(n):
     else:
         return n *factorial(n-1)
 print(factorial(10))
+
+#EXAMPLE 3
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+print(fibonacci(8))
