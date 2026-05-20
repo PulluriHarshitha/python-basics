163cecb0b08148f7b7d07310fb842f8c8f583b0a
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-05-20 18:26:54 +0530

    20-05-26

---
+++ function.py/recursion.py
@@ -0,0 +1,7 @@
+#Recursion means a function calling itself again and again to solve a problem in smaller parts.
+#Instead of using loops, recursion breaks a big problem into smaller versions of the same problem.
+#Basic Idea of Recursion
+#Every recursive function has 2 parts:
+#Base Case → condition to stop recursion
+#Recursive Call → function calls itself
+#Without a base case, the function runs forever and gives an error.
