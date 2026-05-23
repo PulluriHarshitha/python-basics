88029b5836dcd1bb5b8bbd02f94646a7bd3a20cc
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-05-23 22:02:07 +0530

    23-05-26

--- streak.py
+++ streak.py
@@ -2,7 +2,7 @@ ch = input("Enter a character: ")
 if ch in "aeiouAEIOU":
      print("Vowel")
 # else:
-#     print("Consonant")
+     print("Consonant")
 
 ##palindrome num
 num = int(input("Enter a number: "))
