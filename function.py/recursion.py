58b1b2ec9e35ccefbd594ab1fa90a1ec09217e14
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-05-26 19:54:56 +0530

    26-05-26

--- function.py/recursion.py
+++ function.py/recursion.py
@@ -128,3 +128,10 @@ def countdown(n):
     print(n)
     return countdown(n - 1)
 countdown(10)
+
+#fib towords tail recursion
+def fib_tail(n, a=0, b=1):
+    if n == 0:
+        return a
+    return fib_tail(n - 1, b, a + b)
+print(fib_tail(8))
