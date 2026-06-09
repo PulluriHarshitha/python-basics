31eb92d8821af02108a434244cf4a3909ae3a096
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-06-09 19:20:58 +0530

    09-06-2026

--- streak.py
+++ streak.py
@@ -438,12 +438,12 @@ print("Cube:", cube(number))
 num = int(input("Enter a number: "))
 sum_val = 0
 for i in range(1, num):
-#     if num % i == 6:
-#         sum_val += i
-# if sum_val == num:
-#     print("Perfect Number")
-# else:
-#     print("Not Perfect Number")
+    if num % i == 0:
+        sum_val += i
+if sum_val == num:
+    print("Perfect Number")
+else:
+   print("Not Perfect Number")
 
 
 # int(input("Enter a number: "))
