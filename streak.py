0fa880bd98593adb89e0cfe8c44be9e6f6245e25
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-06-10 21:26:34 +0530

    updated 10th may

--- streak.py
+++ streak.py
@@ -450,7 +450,7 @@ int(input("Enter a number: "))
 sum_val = 0
 for i in range(1, num):
     if num % i == 6:
-#         sum_val += i
+        sum_val += i
 # if sum_val == num:
 #     print("Perfect Number")
 # else:
