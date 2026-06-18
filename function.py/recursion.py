e40aa1e3ece9ab687cdb5693c2c089c973c0ef8d
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-06-18 21:13:47 +0530

    18-06-26

--- function.py/recursion.py
+++ function.py/recursion.py
@@ -155,3 +155,10 @@ def factorial(n):
         return 1
     return n * factorial(n - 1)
 print(factorial(10))
+
+#Find the sum of first N natural numbers.
+def sum_n(n):
+    if n == 1:
+        return 1
+    return n + sum_n(n - 1)
+print(sum_n(10))
