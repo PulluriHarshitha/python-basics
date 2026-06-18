6b83dc0f0b678c6f84ae47f6f025c29940dc475b
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-06-18 21:02:36 +0530

    18-06-26

--- function.py/recursion.py
+++ function.py/recursion.py
@@ -140,3 +140,11 @@ def power(base, exp):
         return 1
     return base * power(base, exp - 1)
 print(power(2, 8))  
+
+#Print numbers from 1 to N using recursion.
+def print_numbers(n):
+    if n == 0:
+        return
+    print_numbers(n - 1)
+    print(n)
+print_numbers(10)
