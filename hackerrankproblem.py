50e28108a5c892ed68de4d973952c7b9ed3317c6
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-06-23 22:17:34 +0530

    23-06-26

--- hackerrankproblem.py
+++ hackerrankproblem.py
@@ -202,8 +202,10 @@
 #     print((c * (2 * (thickness - i) - 1)).rjust5(6 * thickness))
 
 #TEXT WRAP
-import textwrap
-string = input()
-max_width = int(input("Enter max width: "))
-result = textwrap.fill(string, 4)
-print(result)
+# import textwrap
+# string = input()
+# max_width = int(input("Enter max width: "))
+# result = textwrap.fill(string, 4)
+# print(result)
+
+
