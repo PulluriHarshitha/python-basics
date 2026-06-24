bbfe07ce9f2d9b3674f575e393a5de00f514dc8d
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-06-24 22:25:33 +0530

    24-06-26

--- streak.py
+++ streak.py
@@ -474,7 +474,7 @@ list2 = ["coke", "charger", "student","software","patient"]
 join_list = []
 for i in range(len(list1)):
      join_list.append(list1[i])
-#     join_list.append(list2[i])
+     join_list.append(list2[i])
 # print(join_list) 
 
 # a = 10
