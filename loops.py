630d4c6ebfdbb8c26ed790e0c20e46a00a59dc4a
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-07-10 15:27:58 +0530

    10-07-26

--- loops.py
+++ loops.py
@@ -39,3 +39,18 @@ fact = 1
 for i in range(1,n+1):
     fact = fact*i #fact = 1*1=1, fact=1*2=2, fact=2*3=6, fact=6*4=24, fact=24*5=120
 print("Factorial", fact)
+
+#ARMSTRONG NUMBERs:is anumber that is equal to the sum of cubes of its digits. each digit is raised to the power of total number of digits.For example, 153 is an armstrong number because 1^3 + 5^3 + 3^3 = 153
+n = int(input())
+n_dup= n #153
+org = n #org = 153
+c = len(str(n)) #c = 3
+sumD = 0
+while(n!=0):
+    dig = n%10 #3,5,1
+    sumD += dig**c  #153
+    n=n//10 #0
+if(org == sumD):
+    print("Armstrong number")
+else:
+    print("Not an armstrong number")
