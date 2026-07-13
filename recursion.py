d51c042321381bc527def63fd6adbc541e883c0b
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-07-13 15:19:20 +0530

    13-07-26

--- recursion.py
+++ recursion.py
@@ -13,8 +13,16 @@
 #     print(n,end=" ")
 #     num(4)
 
-def sumD(n):
-    if n==0:
-        return 0
-    return n%10+sumD(n//10)
-print(sumD(1234))  #1+2+3+4=10        
+#sum of digits of a number using recursion
+# def sumD(n):
+#     if n==0:
+#         return 0
+#     return n%10+sumD(n//10)
+# print(sumD(1234))  #1+2+3+4=10   
+
+#counting the number of digits in a number using recursion
+# def countD(n):
+#     if n==0:
+#         return 0
+#     return 1+countD(n//10)
+# print(countD(1234567890))  #10 digits
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+def sumD(n):
+#     if n==0:
+#         return 0
+#     return n%10+sumD(n//10)
+# print(sumD(1234))  #1+2+3+4=10   
