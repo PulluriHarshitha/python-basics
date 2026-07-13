0851e213a9690f2e26693a91755e689ddbddc504
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-07-13 15:05:23 +0530

    13-07-26

--- recursion.py
+++ recursion.py
@@ -1,8 +1,20 @@
 #recursion 1 to n numbers
-def print_numbers(n, i=1):
-    if i > n:
-        return
-    print(i)
-    print_numbers(n, i + 1)
-n = int(input())
-print_numbers(n)
+# def print_numbers(n, i=1):
+#     if i > n:
+#         return
+#     print(i)
+#     print_numbers(n, i + 1)
+# n = int(input())
+# print_numbers(n)
+
+# def num(n):
+#     if n==0:
+#         return
+#     print(n,end=" ")
+#     num(4)
+
+def sumD(n):
+    if n==0:
+        return 0
+    return n%10+sumD(n//10)
+print(sumD(1234))  #1+2+3+4=10        
