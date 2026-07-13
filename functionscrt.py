aae3b39806766c43ab164137f6bd85d88c2eda88
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-07-13 10:14:51 +0530

    13-07-26

---
+++ functionscrt.py
@@ -0,0 +1,9 @@
+def appu():   #creating a function
+    print("i am appu")
+def bunty():
+    appu()    #calling a function inside another function
+    print("i am bunty")
+def chintu():
+    bunty()
+    print("i am chintu")
+chintu()
