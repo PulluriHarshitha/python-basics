2c8394d8b35d14f4b9ccac0e62e3bff9ed69ba93
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-07-22 15:47:03 +0530

    22-07-2026

--- streak.py
+++ streak.py
@@ -628,23 +628,33 @@ print(twoSum(nums, target))
 #     e1.wag_sal(500)
 
 #calculate even numbers maximum no.of subarrays with size "k"
-arr = list(map(int, input("Enter array elements: ").split()))
-k = int(input("Enter k: "))
-count = 0
-sum = 0
-for i in range(k):
-    if arr[i] % 2 == 0:
-        count += 1
-        sum = sum+arr[i]
-print(count)
-for i in range(k, len(arr)):
-    if arr[i - k] % 2 == 0:
-        sum = sum-arr[i-k]
-        count -= 1
-    if arr[i] % 2 == 0:
-        sum = sum+arr[i]
-        count += 1
-    print(count)
+# arr = list(map(int, input("Enter array elements: ").split()))
+# k = int(input("Enter k: "))
+# count = 0
+# sum = 0
+# for i in range(k):
+#     if arr[i] % 2 == 0:
+#         count += 1
+#         sum = sum+arr[i]
+# print(count)
+# for i in range(k, len(arr)):
+#     if arr[i - k] % 2 == 0:
+#         sum = sum-arr[i-k]
+#         count -= 1
+#     if arr[i] % 2 == 0:
+#         sum = sum+arr[i]
+#         count += 1
+#     print(count)
 
-
+s = input("Enter a string: ")
+vowels = 0
+consonants = 0
+for ch in s:
+    if ch.isalpha():
+        if ch.lower() in "aeiou":
+            vowels += 1
+        else:
+            consonants += 1
+print("Vowels =", vowels)
+print("Consonants =", consonants)
 
