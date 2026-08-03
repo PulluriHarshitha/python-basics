289905f93b8ace77817ef9b71821a3e07dfc83ac
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-08-03 10:27:34 +0530

    03-08-2026

--- streak2.py
+++ streak2.py
@@ -1,8 +1,9 @@
- ch = input("Enter a character: ")
- if ch in "aeiouAEIOU":
+ch = input("Enter a character: ")
+if ch in "aeiouAEIOU":
       print("Vowel")
 else:
-     print("Consonant")  
+     print("Consonant") 
+      
 #palindrome num
 # num = int(input("Enter a number: "))
 # temp = num
