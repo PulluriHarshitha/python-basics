072a9097c7f3c9409f7240521d9bffa6c0ba4f19
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-08-04 15:44:56 +0530

    04-08-2026

--- streak2.py
+++ streak2.py
@@ -569,7 +569,7 @@ else:
 list = [1,2,3,5,7,9,14]
 left = 0
 right = len(list) -1
-# while left < right:
+while left < right:
 #     list[left],list[right] = list[right],list[left]
 #     left += 1
 #     right -= 1
