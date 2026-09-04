f38c7d09e00e11f29a83ac8b30a41f717872e590
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-09-04 23:02:05 +0530

    Update streak2.py

--- streak2.py
+++ streak2.py
@@ -28,7 +28,7 @@ if text == text[::-1]:
 num = int(input("Enter a number: "))
  flag = 0
 if num > 1:
-#      for i in range(2, num):
+     for i in range(2, num):
 #         if num % i == 0:
 #             flag = 1
 #             break
