ed030a80e4b67be479f35591dba6834b2bdd90b8
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-09-07 23:00:49 +0530

    Update streak.py

--- streak.py
+++ streak.py
@@ -18,7 +18,7 @@ else:
        print("Not a palindrome")
 
 
-#  palindrome string
+ palindrome string
 # text = input("Enter a string: ")
 # if text == text[::-1]:
 #      print("Palindrome string")
