0bfa6efdf3f3060f0d44c5331eebeceb37f55381
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-09-08 22:53:16 +0530

    Update streak.py

--- streak.py
+++ streak.py
@@ -20,7 +20,7 @@ else:
 
  palindrome string
 # text = input("Enter a string: ")
-# if text == text[::-1]:
+if text == text[::-1]:
 #      print("Palindrome string")
 # else:
 #    print("Not a palindrome")
