b827f799151ed0abb7aa70ad58646bb6d68fe2ba
pulluri harshitha <pulluriharshitha90@gmail.com>
2026-09-11 11:22:37 +0530

    11-09-26

--- streak2.py
+++ streak2.py
@@ -30,14 +30,14 @@ flag = 0
 if num > 1:
      for i in range(2, num):
         if num % i == 0:
-#             flag = 1
-#             break
-#      if flag == 0:
-#          print("Prime number")
-#      else:
-#          print("Not a prime number")
-# else:
-#     print("Not a prime number")
+            flag = 1
+            break
+     if flag == 0:
+         print("Prime number")
+     else:
+         print("Not a prime number")
+else:
+    print("Not a prime number")
 
 # #armstrong num
 # num = int(input("Enter a number: "))
