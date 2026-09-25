0b5af518e0cc96eaa1c8a09ae6bb83db27998329
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-09-25 22:55:44 +0530

    Update streak2.py

--- streak2.py
+++ streak2.py
@@ -47,7 +47,7 @@ sum = 0
 while num > 0:
       digit = num % 10
       sum = sum + digit**3
-#      num = num // 10
+     num = num // 10
 #      if temp == sum:
 #       print("Armstrong number")
 # else:
