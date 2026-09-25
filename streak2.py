460b154721c3cdd8b979a0e7073f4b4ca6de450f
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-09-25 22:54:57 +0530

    Update streak2.py

--- streak2.py
+++ streak2.py
@@ -45,7 +45,7 @@ num = int(input("Enter a number: "))
 temp = num
 sum = 0
 # while num > 0:
-#      digit = num % 10
+      digit = num % 10
 #      sum = sum + digit**3
 #      num = num // 10
 #      if temp == sum:
